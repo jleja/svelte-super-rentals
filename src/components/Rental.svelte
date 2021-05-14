528e1499db7428c605c46d166470faf48da840ee
John Leja <john.leja@dockyard.com>
2021-05-14 17:17:55 -0400

    Part 1 - Working With Data

--- src/components/Rental.svelte
+++ src/components/Rental.svelte
@@ -1,36 +1,45 @@
 <script>
   import Map from './Map.svelte';
   import RentalImage from './RentalImage.svelte';
+  
+  export let image;
+  export let title;
+  export let owner;
+  export let category;
+  export let city;
+  export let bedrooms;
+  export let location;
 
-  const imageAttrs = {
-		src: "https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg",
-    alt: "A picture of Grand Old Mansion"
-	};
+  const COMMUNITY_CATEGORIES = ['Condo', 'Townhouse', 'Apartment'];
+  const type = COMMUNITY_CATEGORIES.includes(category) ? 'Community' : 'Standalone';
 </script>
 
 <article class="rental">
-  <RentalImage {...imageAttrs} />
+  <RentalImage
+    src={image}
+    alt="A picture of {title}"
+  />
   <div class="details">
-    <h3>Grand Old Mansion</h3>
+    <h3>{title}</h3>
     <div class="detail owner">
-      <span>Owner:</span> Veruca Salt
+      <span>Owner:</span> {owner}
     </div>
     <div class="detail type">
-      <span>Type:</span> Standalone
+      <span>Type:</span> {type}
     </div>
     <div class="detail location">
-      <span>Location:</span> San Francisco
+      <span>Location:</span> {city}
     </div>
     <div class="detail bedrooms">
-      <span>Number of bedrooms:</span> 15
+      <span>Number of bedrooms:</span> {bedrooms}
     </div>
   </div>
   <Map
-    lat={"37.7749"}
-    lng={"-122.4194"}
-    zoom={"9"}
-    width={"150"}
-    height={"150"}
-    alt={"A map of Grand Old Mansion"}
+    lat={location.lat}
+    lng={location.lng}
+    zoom="9"
+    width="150"
+    height="150"
+    alt="A map of {title}"
   />
 </article>
