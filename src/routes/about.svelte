6b48fa8c1c6464a80761e0916b84c1cb4816c469
John Leja <john.leja@dockyard.com>
2021-05-14 12:16:21 -0400

    Part 1 - Building Pages

---
+++ src/routes/about.svelte
@@ -0,0 +1,10 @@
+<div class="jumbo">
+  <div class="right svelte-logo"></div>
+  <h2>About Super Rentals</h2>
+  <p>
+    The Super Rentals website is a delightful project created to explore Svelte.
+    By building a property rental site, we can simultaneously imagine traveling
+    AND building Svelte applications.
+  </p>
+  <a sveltekit:prefetch href="contact" class="button">Contact Us</a>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,4 +2,5 @@
   <div class="right svelte-logo"></div>
   <h2>Welcome to Super Rentals!</h2>
   <p>We hope you find exactly what you're looking for in a place to stay.</p>
+  <a sveltekit:prefetch href="about" class="button">About Us</a>
 </div>
