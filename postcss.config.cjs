ca94b91f38d77024d82b887b313484acfd75bf0e
John Leja <john.leja@dockyard.com>
2021-07-02 14:16:01 -0400

    Resolve svelte-add issue with tailwind

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,22 +1,19 @@
-const tailwindcss = require("tailwindcss");
 const autoprefixer = require("autoprefixer");
 const cssnano = require("cssnano");
+const tailwindcss = require("tailwindcss");
 
 const mode = process.env.NODE_ENV;
+
 const dev = mode === "development";
-
-module.exports = {
+const config = {
 	plugins: [
-		// Some plugins, like postcss-nested, need to run before Tailwind
-		
 		tailwindcss,
-		
-		// But others, like autoprefixer, need to run after
+		autoprefixer(),
 
-		autoprefixer,
-		
 		!dev && cssnano({
 			preset: "default",
 		}),
 	],
 };
+
+module.exports = config;
