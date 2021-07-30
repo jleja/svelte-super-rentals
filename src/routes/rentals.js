d93106c2f5b912c7158c2c735ad5ef0a32e96cf9
John Leja <john.leja@dockyard.com>
2021-07-30 16:46:52 -0400

    centralize prisma client ref / update prisma client

--- src/routes/rentals.js
+++ src/routes/rentals.js
@@ -1,6 +1,4 @@
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import prisma from '$lib/db'
 
 export async function get() {
 	const rentals = await prisma.rental.findMany({
--- src/routes/rentals/[rentalId].json.js
+++ src/routes/rentals/[rentalId].json.js
@@ -1,6 +1,4 @@
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import prisma from '$lib/db'
 
 export async function get({ params }) {
 	const rental = await prisma.rental.findUnique({
